021d994c1fca213cc035c2810b19238d337e3991
Aliz <fad127alireza2@gmail.com>
2020-11-25 16:43:15 +0330

    return neighbors

--- application.py
+++ application.py
@@ -8,11 +8,13 @@ def generateGraph():
     diamond = list() #List for Location diamond
     home = list() #Lisr fot location Homes
     numbers = ["0","1","2","3","4"]
-    with open("Maps/map3/map.txt" , "r") as fin:
+    
+    with open("Maps/map1/map.txt" , "r") as fin:
 
         for line in fin:
             maps.append(list(line.strip()))
         
+        #Find walls, diamond, agent, homes
         for i in range(0 , len(maps)):
             for j in range(0 , len(maps)):
                 if maps[i][j] == '*':
@@ -24,27 +26,32 @@ def generateGraph():
                 elif maps[i][j] == 'a':
                     home.append(tuple((i,j)))
 
+        #Add edges ==> row
         for i in range (0 , len(maps)):
             for j in range (0 , len(maps)):
                 if tuple((i,j)) not in walls and tuple((i,j+1)) not in walls:
                     if j+1 < len(maps):
                         G.add_edge(f"{i},{j}",f"{i},{j+1}")
         
+        #Add edges ==> Column
         for j in range (0 , len(maps)):
             for i in range (0 , len(maps)):
                 if tuple((i,j)) not in walls and tuple((i+1,j)) not in walls:
                     if i+1 < len(maps):
                         G.add_edge(f"{i},{j}",f"{i+1},{j}")
-
+    
+    #Save picture of graph
     nx.draw(G,with_labels=True)
     plt.savefig("test.png") # save as png
     plt.show() # display
+    
+    return(G)
                     
 
+def Neighbors(G,node):
+    return (list(nx.neighbors(G,node)))
 
-    # print('walls: ',walls)
-    # print('diamond: ',diamond)
-    # print('agent: ',agent)
-    # print('home: ',home)
 
-generateGraph()
+graph = generateGraph()
+print(Neighbors(graph,'1,1'))
+
