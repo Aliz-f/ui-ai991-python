d58c15e2dfe44def6eafa9b75ee551808188507d
Aliz <fad127alireza2@gmail.com>
2020-11-26 18:05:10 +0330

    add dumpAgent

--- utils.py
+++ utils.py
@@ -4,6 +4,12 @@ from anytree import Node , RenderTree
 from anytree.render import ContStyle
 from base import Action
 
+maps = list() #List for Maps
+with open("ui-ai991-python/Maps/map1/map.txt" , "r") as fin:
+    for line in fin:
+        maps.append(list(line.strip()))
+
+
 class nodeTree (Node):
     def __init__(self, child , parent, label):
         super().__init__(child , parent=parent)
@@ -39,57 +45,50 @@ class nodeTree (Node):
                     self.action = Action.DOWN
                     break
                 
-def generateGraph():
+def generateGraph(maps):
     G=nx.Graph() #Create Graph
-    maps = list() #List for Maps
     walls = list() #List for locations Walls 
     diamond = list() #List for Location diamond
     home = list() #Lisr fot location Homes
     numbers = ["0","1","2","3","4"]
-    
-    with open("ui-ai991-python/Maps/map1/map.txt" , "r") as fin:
+    #Find walls, diamond, agent, homes
+    for i in range(0 , len(maps)):
+        for j in range(0 , len(maps)):
+            if maps[i][j] == '*':
+                walls.append(tuple((i,j)))
+            elif maps[i][j] in numbers:
+                diamond.append(tuple((i,j)))
+            elif maps[i][j] == 'A':
+                agent = f"{i},{j}"
+            elif maps[i][j] == 'a':
+                home.append(tuple((i,j)))
 
-        for line in fin:
-            maps.append(list(line.strip()))
-        
-        #Find walls, diamond, agent, homes
-        for i in range(0 , len(maps)):
-            for j in range(0 , len(maps)):
-                if maps[i][j] == '*':
-                    walls.append(tuple((i,j)))
-                elif maps[i][j] in numbers:
-                    diamond.append(tuple((i,j)))
-                elif maps[i][j] == 'A':
-                    agent = f"{i},{j}"
-                elif maps[i][j] == 'a':
-                    home.append(tuple((i,j)))
-
-        #Add edges ==> row
-        for i in range (0 , len(maps)):
-            for j in range (0 , len(maps)):
-                if tuple((i,j)) not in walls and tuple((i,j+1)) not in walls:
-                    if j+1 < len(maps):
-                        G.add_edge(f"{i},{j}",f"{i},{j+1}")
-        
-        #Add edges ==> Column
+    #Add edges ==> row
+    for i in range (0 , len(maps)):
         for j in range (0 , len(maps)):
-            for i in range (0 , len(maps)):
-                if tuple((i,j)) not in walls and tuple((i+1,j)) not in walls:
-                    if i+1 < len(maps):
-                        G.add_edge(f"{i},{j}",f"{i+1},{j}")
+            if tuple((i,j)) not in walls and tuple((i,j+1)) not in walls:
+                if j+1 < len(maps):
+                    G.add_edge(f"{i},{j}",f"{i},{j+1}")
+        
+    #Add edges ==> Column
+    for j in range (0 , len(maps)):
+        for i in range (0 , len(maps)):
+            if tuple((i,j)) not in walls and tuple((i+1,j)) not in walls:
+                if i+1 < len(maps):
+                    G.add_edge(f"{i},{j}",f"{i+1},{j}")
     
     #Save picture of graph
     nx.draw(G,with_labels=True)
     plt.savefig("resault.png") # save as png
     plt.show() # display
     
-    return(G , agent)
+    return(G, agent, diamond, home)
                     
 def Neighbors(G,node):
     return (list(nx.neighbors(G,node)))
 
-def root_tree (G,node):
-    root = nodeTree(node,None,None)
+def root_tree (G,start):
+    root = nodeTree(start,None,None)
     return root
     
 def expand_tree(G,parent):
@@ -102,7 +101,8 @@ def expand_tree(G,parent):
 
 
 
-graph,agent = generateGraph()
+graph,agent, diamond , home = generateGraph(maps)
+
 # print(Neighbors(graph,'1,1'))
 root = root_tree(graph,agent)
 our_list = expand_tree(graph,root)
