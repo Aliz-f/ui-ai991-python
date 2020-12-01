9df4a1143985aba270cdf77fe4a90d4b56e24659
Aliz <fad127alireza2@gmail.com>
2020-12-01 14:14:22 +0330

    delet some files

--- Without_Decision_time_limit/DumpAgent/main.py
+++
@@ -1,5 +0,0 @@
-from dumpAgent import DumpAgent
-
-if __name__ == '__main__':
-    winner = DumpAgent().play()
-    print("WINNER: " + winner)
--- Without_Decision_time_limit/SmartAgent/main.py
+++
@@ -1,5 +0,0 @@
-from smartAgent import SmartAgent
-
-if __name__ == '__main__':
-    winner = SmartAgent().play()
-    print("WINNER: " + winner)
